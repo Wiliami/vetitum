874a3225e507c5df1e17f9a94c4631e54ee88609
wiliami <wiliaup97@gmail.com>
2026-02-23 17:29:14 -0400

    feat: add new feature

--- src/functions/get-users.ts
+++ src/functions/get-users.ts
@@ -1,9 +1,25 @@
- export async function getUsers() {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+export async function getUsers() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
 
-    if(!response.ok) {
-        throw new Error('Erro ao buscar usuários.')
+        if(!response.ok) {
+            throw new Error(`Erro ao buscar usuários: ${response.statusText}`)
+        }
+
+        return await response.json();
+    } catch (err) {
+        console.error('Interno server error:', err)
     }
-
-    return await response.json();
 }
+
+async function getData() {
+    try {
+        const data = await getUsers()
+        
+        return data;
+    } catch (err) {
+        console.error('Interno server error:', err)
+    }
+}   
+
+console.log(getData())
